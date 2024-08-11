c5350109cb377ae2d4fecc34e88ad27becc2d9bc
Hooby <92258189+Youcu@users.noreply.github.com>
2024-08-11 11:07:52 +0900

    Update 2_📑_Word_Master.py

--- pages/2_📑_Word_Master.py
+++ pages/2_📑_Word_Master.py
@@ -16,7 +16,7 @@ df = pd.read_csv(PATH+file_name) # col_list = [ Day, Word, Mean, Rand ]
 df['Day'] = df['Day'].astype(float)
 df = pd.DataFrame(df) # CSV to DataFrame
 
-clean_df(df)
+#clean_df(df)
 df = count_df(df)
 
 # Define Dictionary
