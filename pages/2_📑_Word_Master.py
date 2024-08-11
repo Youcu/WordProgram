32b3d495fc494aa1d4c065c75600cf844d1cdc66
Hooby <92258189+Youcu@users.noreply.github.com>
2024-08-11 12:22:39 +0900

    Update 2_📑_Word_Master.py

--- pages/2_📑_Word_Master.py
+++ pages/2_📑_Word_Master.py
@@ -353,9 +353,9 @@ if condition_btn(btn_list, 1, 'btn_list'):
                 st.rerun()  # Use st.rerun instead of st.experimental_rerun
         elif is_in_session(1, 'is_stat'):
             st.header('Hidden Sorted')
-            st.write(st.session_state.restored_df)
+            #st.write(st.session_state.restored_df)
             restored_df = sorting(st.session_state.restored_df, 0) # OK
-            st.write(restored_df)
+            #st.write(restored_df)
             # Check if the edited dataframe is different from the original dataframe
             if check_session_update(restored_df, 'restored_df') : 
                 st.rerun()  # Use st.rerun instead of st.experimental_rerun
